7bbc2a853a5442e418cc3985a249cdbb5288d4bc
mohamim360 <mohamim360@gmail.com>
2023-12-24 01:26:04 +0600

    edit deprecated option

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
   },
   webpack(config) {
     config.experiments = {
